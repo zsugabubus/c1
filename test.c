c6014c18a3dfe876e9d9508711e7fa812ebd67d9
zsugabubus <zsugabubus@users.noreply.github.com>
2019-05-04 03:22:25 +0200

    Initial commit

---
+++ test.c
@@ -0,0 +1,315 @@
+/*
+ * Copyright (C) 2019 zsugabubus
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#define _GNU_SOURCE
+#ifndef TEST_NOLIBSEGFAULT
+# include <dlfcn.h> /* dlopen */
+#else
+# include <execinfo.h> /* backtrace, backtrace_symbols_fd */
+#endif
+#include <limits.h> /* LONG_MAX */
+#include <signal.h> /* signal */
+#include <sys/mman.h> /* memfd_create */
+#include <time.h> /* clock_gettime */
+
+#include "test.h"
+
+int test_case_depth = 0;
+struct test_case_info test_case_info[7];
+struct test_test_info *currtest;
+
+#define US_NS 1000ul
+#define MS_NS 1000000ul
+#define SEC_NS 1000000000ul
+
+static void cat(int fd) {
+	char buf[(1 << 12)];
+	ssize_t len;
+	off_t offset;
+
+	for (offset = 0;(len = pread(fd, buf, sizeof(buf), offset)) > 0;offset += sizeof(buf))
+		write(STDERR_FILENO, buf, len);
+}
+
+static unsigned long nstohtime(unsigned long ns) {
+	if (ns < 10 * US_NS)
+		return ns;
+	else if (ns < 10 * MS_NS)
+		return ns / US_NS;
+	else if (ns < 10 * SEC_NS)
+		return ns / MS_NS;
+	else
+		return ns / SEC_NS;
+}
+
+static char const *nstohunit(unsigned long ns) {
+	if (ns < 10 * US_NS)
+		return "ns";
+	else if (ns < 10 * MS_NS)
+		return "us";
+	else if (ns < 10 * SEC_NS)
+		return "ms";
+	else
+		return "s";
+}
+
+static unsigned long tstons(struct timespec const *const ts) {
+	return ts->tv_sec * SEC_NS + ts->tv_nsec;
+}
+
+static void tsdiff(struct timespec *end, struct timespec *start, struct timespec *result) {
+	result->tv_sec  = end->tv_sec  - start->tv_sec  + (end->tv_nsec < start->tv_nsec);
+	result->tv_nsec = end->tv_nsec - start->tv_nsec + (end->tv_nsec < start->tv_nsec ? SEC_NS : 0);
+}
+
+static void print_hline(int n, char ch) {
+	int i;
+
+	for (i = 0;i < n;++i)
+		putc(ch, stdout);
+	putc('\n', stdout);
+}
+
+#ifdef TEST_NOLIBSEGFAULT
+void sighandler(int sig, siginfo_t *info, void *ucontext)
+{
+	void *array[50];
+	int size;
+
+	fprintf(stderr, "*** %s, address is %p\n\n",
+		strsignal(sig), info->si_addr);
+
+	size = backtrace(array, 50);
+
+	fprintf(stderr, "Backtrace:\n");
+	backtrace_symbols_fd(array, size, STDERR_FILENO);
+
+	exit(EXIT_FAILURE);
+}
+#endif
+
+int main(int argc, char **argv) {
+
+	enum { test_unit, test_total, test_passed, test_failed, test_skipped, test_result_count };
+	unsigned stat[test_result_count] = {0};
+	int longest_name = 0;
+	int longest_unit = 0;
+#ifndef TEST_NOLIBSEGFAULT
+	void *dlhandle;
+
+	dlhandle = dlopen("/lib/libSegFault.so", RTLD_LAZY);
+	if (!dlhandle)
+		/* Don't treat it fatal error. */
+		perror("dlopen");
+#else
+	struct sigaction sigact;
+
+	sigact.sa_sigaction = sighandler;
+	sigact.sa_flags = SA_RESTART | SA_SIGINFO;
+
+	if (sigaction(SIGSEGV, &sigact, NULL)) {
+		perror("sigaction");
+		exit(EXIT_FAILURE);
+	}
+#endif
+
+	for (currtest = &__start_test;currtest < &__stop_test;++currtest) {
+		int len;
+		
+		if (currtest->run) {
+			++stat[test_total];
+
+			if (!currtest->desc)
+				continue;
+
+			len = strlen(currtest->desc);
+			if (len > longest_name)
+				longest_name = len;
+
+		} else {
+			++stat[test_unit];
+			len = 5 + strlen(currtest->desc) + strlen(currtest->file) + 4;
+			if (len > longest_unit)
+				longest_unit = len;
+		}
+	}
+
+	fprintf(stdout, "found %u tests in %u units\n",
+		stat[test_total], stat[test_unit]);
+
+	for (currtest = &__start_test;currtest < &__stop_test;++currtest) {
+		unsigned long *total_ns;
+		char succ_info[50];
+		int argi;
+		int skip;
+
+		if (!currtest->run) {
+			fprintf(stdout, "\nunit " ANSI_BOLD "%s" ANSI_RESET " (%s):\n",
+				currtest->desc, currtest->file);
+
+			print_hline(longest_unit, '-');
+			continue;
+		}
+
+		fprintf(stdout, "test " ANSI_BOLD "%s" ANSI_RESET " ... %*s",
+			currtest->desc,
+			longest_name - (int)strlen(currtest->desc), "");
+		fflush(stdout);
+
+		for (skip = 0, argi = 1;argi < argc;++argi) {
+			char *filter = argv[argi];
+			int should_skip = 0;
+			switch (filter[0]) {
+			case '+':
+				++filter;
+				break;
+			case '-':
+				++filter;
+				should_skip = 1;
+				break;
+			}
+
+			if (1 == argi)
+				skip = !should_skip;
+
+			if (strstr(currtest->desc, filter))
+				skip = should_skip;
+		}
+
+		if (skip || 0 == currtest->repeat) {
+			++stat[test_skipped];
+			fprintf(stdout, ANSI_BLUE ANSI_BOLD "skipped" ANSI_RESET "\n");
+			continue;
+		}
+
+		currtest->outfd = memfd_create(currtest->desc, 0);
+		if (-1 == currtest->outfd) {
+			perror("memfd_create");
+			exit(EXIT_FAILURE);
+		}
+
+		total_ns = mmap(NULL, sizeof(*total_ns),
+			PROT_READ | PROT_WRITE,
+			MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+		if (MAP_FAILED == total_ns) {
+			perror("mmap");
+			exit(EXIT_FAILURE);
+		}
+
+		switch (fork()) {
+		case -1:
+			perror("fork");
+			exit(EXIT_FAILURE);
+		case 0: {
+			struct timespec ts_start;
+			struct timespec ts_end;
+			unsigned repeat = currtest->repeat;
+
+			/* Run test in forked children. */
+			dup2(currtest->outfd, STDERR_FILENO);
+			dup2(currtest->outfd, STDOUT_FILENO);
+
+			/* Do not buffer output streams. */
+			setvbuf(stdout, NULL, _IONBF, 0);
+			setvbuf(stderr, NULL, _IONBF, 0);
+
+			clock_gettime(CLOCK_MONOTONIC, &ts_start);
+			while (repeat-- > 0)
+				currtest->run();
+			clock_gettime(CLOCK_MONOTONIC, &ts_end);
+
+			tsdiff(&ts_end, &ts_start, &ts_start);
+			*total_ns = tstons(&ts_start);
+
+			exit(EXIT_SUCCESS);
+			break;
+		}
+		default:
+			/* Wait test to be finished. */ \
+			wait(&currtest->status);
+
+			currtest->total_ns = *total_ns;
+
+			++stat[(EXIT_SUCCESS == currtest->status ? test_passed : test_failed)];
+
+			if (EXIT_SUCCESS == currtest->status) {
+				if (currtest->repeat <= 1)
+					sprintf(succ_info, ", %4lu %s",
+						nstohtime(currtest->total_ns),
+						nstohunit(currtest->total_ns));
+				else
+					sprintf(succ_info, ", %4lu %s / %9lu iters = %4lu %s/iter",
+						nstohtime(currtest->total_ns),
+						nstohunit(currtest->total_ns),
+						currtest->repeat,
+						nstohtime((currtest->total_ns + currtest->repeat - 1) / currtest->repeat),
+						nstohunit((currtest->total_ns + currtest->repeat - 1) / currtest->repeat));
+			} else {
+				succ_info[0] = '\0';
+			}
+
+			fprintf(stdout, ANSI_BOLD "%s" ANSI_RESET "%s\n",
+				EXIT_SUCCESS == currtest->status
+				 ? ANSI_GREEN "ok"
+				 : ANSI_RED "FAILED",
+				succ_info);
+
+			if ('I' == TEST_OUTPUT) {
+				if (EXIT_SUCCESS != currtest->status) {
+					cat(currtest->outfd);
+				}
+			}
+		}
+
+	}
+
+	if ('A' == TEST_OUTPUT && stat[test_failed] > 0) {
+		print_hline(longest_unit, '=');
+
+		for (currtest = &__start_test;++currtest < &__stop_test;) {
+			if (EXIT_SUCCESS != currtest->status) {
+				fprintf(stderr, "test " ANSI_BOLD "%s" ANSI_RESET " (%s:%u):\n",
+					currtest->desc, currtest->file, currtest->line);
+				cat(currtest->outfd);
+				fprintf(stderr, "\n");
+			}
+		}
+	}
+
+	print_hline(longest_unit, '=');
+
+	fprintf(stdout,
+		ANSI_BOLD "test result" ANSI_RESET ": " ANSI_BOLD "%s" ANSI_RESET ". "
+		ANSI_GREEN "%u passed"  ANSI_RESET "; "
+		ANSI_RED   "%u failed"  ANSI_RESET "; "
+		ANSI_BLUE  "%u skipped" ANSI_RESET "\n",
+		0 == stat[test_failed]
+		 ? ANSI_GREEN "ok"
+		 : ANSI_RED "FAILED",
+		stat[test_passed],
+		stat[test_failed],
+		stat[test_skipped]);
+
+#ifndef TEST_NOLIBSEGFAULT
+	dlclose(dlhandle);
+#endif
+
+	exit(0 == stat[test_failed]
+		 ? EXIT_SUCCESS
+		 : EXIT_FAILURE);
+}
+/* vim:set ft=c ts=4 sw=4 noet: */
---
+++ test.h
@@ -0,0 +1,199 @@
+/*
+ * Copyright (C) 2019 zsugabubus
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#ifndef TEST_H
+#define TEST_H
+
+#include <fcntl.h> /* O_WRONLY */
+#include <stdio.h> /* fprintf, perror */
+#include <stdlib.h> /* exit */
+#include <string.h> /* strcmp, memcmp */
+#include <sys/wait.h> /* wait */
+#include <unistd.h> /* fork, dup2 */
+
+#ifndef TEST_OUTPUT
+# define TEST_OUTPUT 'A'
+#endif
+
+#ifndef TEST_CASESEP
+# define TEST_CASESEP " / "
+#endif
+
+#ifndef TEST_NOANSIES
+# define ANSI_BOLD  "\x1b[1m"
+# define ANSI_RESET "\x1b[0m"
+# ifndef TEST_NOCOLORS
+#  define ANSI_RED "\x1b[31m"
+#  define ANSI_GREEN "\x1b[32m"
+#  define ANSI_ORANGE "\x1b[33m"
+#  define ANSI_BLUE "\x1b[34m"
+#  define ANSI_MAGENTA "\x1b[35m"
+# else
+#  define ANSI_RED ""
+#  define ANSI_GREEN ""
+#  define ANSI_ORANGE ""
+#  define ANSI_BLUE ""
+#  define ANSI_MAGENTA ""
+# endif
+#else
+#define ANSI_BOLD  ""
+#define ANSI_RESET ""
+#define ANSI_RED ""
+#define ANSI_GREEN ""
+#define ANSI_ORANGE ""
+#define ANSI_BLUE ""
+#define ANSI_MAGENTA ""
+#endif
+
+struct test_case_info {
+	char const *name;
+	char const *file;
+	unsigned line;
+	off_t outpos;
+};
+
+struct test_test_info {
+	void(*const run)(void)  __attribute__((aligned(16)));
+	char const *const desc;
+	char const *const file;
+	unsigned line;
+	int outfd;
+	int status;
+	unsigned long repeat;
+	unsigned long total_ns;
+};
+
+extern int test_case_depth;
+extern struct test_case_info test_case_info[7];
+extern struct test_test_info *currtest;
+__attribute__((section("test")))
+extern struct test_test_info __start_test, __stop_test;
+
+#define BENCH(name, repeat) \
+	BENCH_(void, name, #name, repeat)
+
+/* Must be static, because we can't generate unique names. */
+#define BENCHD(desc, repeat) \
+	BENCH_(static void, TOKENPASTE(test_at_line_, __LINE__), desc, repeat)
+
+#define TEST(name) \
+	BENCH(name, 1)
+
+#define TESTD(desc) \
+	BENCHD(desc, 1)
+
+#define BENCH_(static_void, name, desc, repeat) \
+	static_void name(void); \
+	TEST_VAR(name) = {name, desc, __FILE__, __LINE__, -1, 0, repeat}; \
+	static_void name(void)
+
+#define CASE(name) \
+	CASED(#name)
+
+#define CASED(case_desc) \
+	assert_msg_(test_case_depth < (sizeof(test_case_info) / sizeof(*test_case_info)), "cannot nest more cases"); \
+	test_case_info[test_case_depth].name = (case_desc); \
+	test_case_info[test_case_depth].file = __FILE__; \
+	test_case_info[test_case_depth].line = __LINE__; \
+	test_case_info[test_case_depth].outpos = lseek(currtest->outfd, 0, SEEK_CUR); \
+\
+	switch (fork()) { \
+	case -1: \
+		perror("fork"); \
+		exit(EXIT_FAILURE); \
+	case 0: { \
+		int i; \
+		++test_case_depth; \
+\
+		fprintf(stderr, "case "); \
+		for (i = 0;i < test_case_depth - 1;++i) \
+			fprintf(stderr, ANSI_BOLD "%s" ANSI_RESET TEST_CASESEP, \
+				test_case_info[i].name); \
+\
+		fprintf(stderr, ANSI_BOLD "%s" ANSI_RESET " (%s:%u):\n", \
+			test_case_info[i].name, \
+			test_case_info[i].file, \
+			test_case_info[i].line \
+		); \
+		goto TOKENPASTE(test_case_run_, __LINE__); \
+	} \
+	default: { \
+		int status; \
+		wait(&status); \
+		if (EXIT_SUCCESS == status) \
+			ftruncate(currtest->outfd, test_case_info[test_case_depth].outpos); \
+		else \
+			exit(EXIT_FAILURE); \
+	} \
+	} \
+	for (;0;exit(EXIT_SUCCESS)) TOKENPASTE(test_case_run_, __LINE__):
+
+#define TEST_VAR(name) \
+	static struct test_test_info test_##name __attribute__((unused)) __attribute__((section("test")))
+
+#define UNIT(name) \
+	UNITD(#name)
+
+#define UNITD(desc) \
+	UNIT_(desc)
+
+#define UNIT_(desc) \
+	TEST_VAR(unit) = {NULL, desc, __FILE__, 0, -1, 0};
+
+#define TOKENPASTE_(a, b) a##b
+#define TOKENPASTE(a, b) TOKENPASTE_(a, b)
+
+#define assert_msg_(c, msg) \
+	if (!(c)) {  \
+		fprintf(stderr, "%s:%u: " ANSI_ORANGE ANSI_BOLD "Assertion" ANSI_RESET " %s\n", \
+			__FILE__, __LINE__, msg); \
+		exit(EXIT_FAILURE); \
+	}
+
+#define assert(c) \
+	assert_msg_(c, "`" #c "' failed.")
+
+#define assert_eq(lhs, rhs) \
+	assert(lhs == rhs);
+
+#define assert_cstreq(lhs, rhs) \
+	assert(0 == strcmp(lhs, rhs));
+
+#define assert_memeq(lhs, rhs, size) \
+	assert(0 == memcmp(lhs, rhs, size));
+
+#define expect_msg_(expected, actual, type, cmp, fmt) \
+do { \
+	type exp = (expected); \
+	type act = (actual); \
+	if (!(cmp)) {  \
+		fprintf(stderr, "%s:%u: " ANSI_ORANGE ANSI_BOLD "Expected" ANSI_RESET " " fmt ", got " fmt ".\n", \
+			__FILE__, __LINE__, exp, act); \
+		exit(EXIT_FAILURE); \
+	} \
+} while (0);
+
+#define expect_int(expected, actual) \
+	expect_msg_(expected, actual, long int const, (exp == act), "%ld")
+
+#define expect_uint(expected, actual) \
+	expect_msg_(expected, actual, long unsigned const, (exp == act), "%lu")
+
+#define expect_str(expected, actual) \
+	expect_msg_(expected, actual, char const*, (0 == strcmp(exp, act)), "`%s'")
+
+#endif /* TEST_H */
+/* vim:set ft=c ts=4 sw=4 noet: */
